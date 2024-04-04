df4fe6436ba1491636e64032f841c68d3a39c917
Adarsh <isadarsh02@gmail.com>
2024-04-04 19:30:16 +0530

    access,refresh tokens, prehook

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose";
-import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -47,5 +47,45 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  //if password is not changed then dont do prehooks
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+//giving custom password check methods
+userSchema.methods.isPasswordCorrectAdarsh = async function (password) {
+  return await bcrypt.compare(password, this.password)
+}
+
+//jwt is a bearer token
+userSchema.methods.generateAccessToken= function(){
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      userName: this.userName,
+      fullName: this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+userSchema.methods.generateRefreshToken= function(){
+  return jwt.sign(
+    {
+      _id: this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
 
 export const User = mongoose.model("User", userSchema);
