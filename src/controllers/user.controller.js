2332a3d4ac13589c355dd648a973fd36a838c7c1
Adarsh <isadarsh02@gmail.com>
2024-04-09 22:25:02 +0530

    media

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,10 +2,12 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res, next) => {
     // console.log("i am the error");
-    return res.status(200).json({
-        message: "this is the point",
-        body: "i am the body"
-    })
+    // return res.status(200).json({
+    //     message: "this is the point",
+    //     body: "i am the body"
+    // })
+    const { fullName, email, userName, password } = req.body;
+    console.log(password);
 });
 
 export {registerUser}
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,24 @@
 // import { Router } from "express";
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+    //inject first the middleware of multer to handle files
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    //then the methods
+    registerUser
+);
 
 export default router;
